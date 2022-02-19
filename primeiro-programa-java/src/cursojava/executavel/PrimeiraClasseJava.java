b6887d9c85f08464ee1c096167318a5e2218bb9a
ceci <ceci@DESKTOP-FFE287H>
2022-02-19 18:19:44 -0300

    criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+/* mains é um método auto executável em java*/
+
+public class PrimeiraClasseJava {
+	public static void main(String[]args) {
+		
+	/*system abaixo escreve texto*/
+		System.out.println("Este é meu primeiro programa em Java. Do curso JavaWEB");	
+		System.out.println("Java é orientado a objeto");
+	}
+}
